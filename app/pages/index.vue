5ec2850836108c0256338d0dd97f2b8c24d7a479
John Read <jr@johnpaulread.com>
2022-12-08 12:50:40 -0800

    update

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,11 @@
 <template>
   <section class="home">
-    <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
-      <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
+    <div
+      class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center"
+    >
+      <div
+        class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden"
+      >
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
 
         <div class="mb-12 xl:mb-0">
@@ -20,7 +24,7 @@
               class="appearance-none mb-36 bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
               type="text"
               name="email"
-              placeholder="your@email.com"
+              placeholder="yoyoyoyoy@email.com"
               aria-label="Email address"
             />
 
@@ -45,44 +49,47 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator';
-import settings from '@/content/settings/general.json';
+import { Component, Vue } from 'nuxt-property-decorator'
+import settings from '@/content/settings/general.json'
 
 @Component({
   // Called to know which transition to apply
   transition() {
-    return 'slide-left';
+    return 'slide-left'
   },
 })
 export default class Home extends Vue {
-  welcomeText = settings.welcomeText;
+  welcomeText = settings.welcomeText
 
   get posts(): Post[] {
-    return this.$store.state.posts;
+    return this.$store.state.posts
   }
 
-  isSignedUp = false;
+  isSignedUp = false
 
   form = {
     email: '',
-  };
+  }
 
   encode(data): string {
     return Object.keys(data)
-      .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
-      .join('&');
+      .map(
+        (key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`,
+      )
+      .join('&')
   }
 
   validEmail(email): boolean {
     // eslint-disable-next-line
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+    const re =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return re.test(email)
   }
 
   async handleSubmit(): Promise<void> {
     if (!this.validEmail(this.form.email)) {
-      this.$refs.emailInput.focus();
-      return;
+      this.$refs.emailInput.focus()
+      return
     }
 
     try {
@@ -90,11 +97,11 @@ export default class Home extends Vue {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: this.encode({ 'form-name': 'signups', ...this.form }),
-      });
+      })
 
-      this.isSignedUp = true;
+      this.isSignedUp = true
     } catch (error) {
-      console.error(error);
+      console.error(error)
     }
   }
 }
